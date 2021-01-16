eb9a115d3cd40c6acc1f24eecaf0967ff922ecca
Connor Cozens <cozcon@gmail.com>
2021-01-16 14:20:13 -0500

    Added user login route & working insert user route.

--- api/routes/users.js
+++ api/routes/users.js
@@ -16,6 +16,39 @@ var config = {
     }
 };
 
+router.get('/login', function(req, res, next) {
+  var pool = new pg.Pool(config);
+
+  var email = req.body.email;
+  // var password = req.body.password;
+
+  pool.connect(function (err, client, done) {
+
+      var finish = function () {
+          done();
+      };
+  
+      if (err) {
+          console.error('could not connect to cockroachdb', err);
+          finish();
+      }
+
+      async.waterfall([
+          function (next) {
+              // Print out account balances.
+              client.query('SELECT * FROM users WHERE email = $1', [email], next);
+          },
+      ],
+      function (err, results) {
+          if (err) {
+              console.error('Error inserting into and selecting from accounts: ', err);
+              finish();
+          }
+          res.send(results.rows[0]);
+      });
+  });
+});
+
 router.get('/', function(req, res, next) {
     var pool = new pg.Pool(config);
 
@@ -23,7 +56,6 @@ router.get('/', function(req, res, next) {
 
         var finish = function () {
             done();
-            process.exit();
         };
     
         if (err) {
@@ -50,10 +82,16 @@ router.get('/', function(req, res, next) {
 router.post('/', function(req, res, next) {
   var pool = new pg.Pool(config);
 
+  console.log(req.body);
+  var email = req.body.email;
+  var age = req.body.age;
+  var country = req.body.country;
+  var current_level = req.body.current_level;
+  var current_xp = req.body.current_xp;
+
   pool.connect(function (err, client, done) {
     var finish = function() {
       done();
-      process.exit();
     };
     if (err) {
       console.error('Could not connect to corkroachdb', err);
@@ -62,7 +100,7 @@ router.post('/', function(req, res, next) {
 
     async.waterfall([
       function(next) {
-        client.query("INSERT into users (email, age, country, current_level, current_xp) VALUES ('cozcon@gmail.com', 75, 'Connor', 1,0);", next);
+        client.query('INSERT into users (email, age, country, current_level, current_xp) VALUES ($1, $2, $3, $4, $5);', [email, age, country, current_level, current_xp], next);
       },
     ],
     function (err, results) {
