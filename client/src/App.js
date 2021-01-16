ec30c52921bef88f7a5c6de650e301e1159919d4
Patrick <patrickjung@rogers.com>
2021-01-16 00:32:34 -0500

    Basic frontend routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Navbar } from './components';
+import './App.css'
+// import logo from './logo.svg';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Router>
+		
+			<Navbar />
+			<div className='contentBody'>
+				<Route exact path='/' render={() =>
+					<div>Landing Page</div>
+				} />
+				<Route exact path='/temp1' render={() =>
+					<div>Temporary Page 1</div>
+				} />
+				<Route exact path='/temp2' render={() =>
+					<div>Temporary Page 2</div>
+				} />
+			</div>
+
+		</Router>
+	);
 }
 
 export default App;
---
+++ client/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Button } from 'react-bootstrap';
+import styles from './Navbar.module.css';
+import cx from 'classnames';
+
+function Navbar() {
+
+    return (
+
+        <nav className={cx("navbar navbar-expand-lg navbar-light shadow-sm bg-light rounded", styles.navbar)}>
+            <Button href="/" variant="light" className={styles.menuButton}>
+                Home
+            </Button>
+            <Button href="/temp1" variant="light" className={styles.menuButton}>
+                Page1
+            </Button>
+            <Button href="/temp2" variant="light" className={styles.menuButton}>
+                Page2
+            </Button>
+        </nav>
+
+    )
+}
+
+export default Navbar;
