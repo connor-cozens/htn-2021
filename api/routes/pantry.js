32ae41b3767988e2cf9b6dbc9853220cfaa2945e
matthewnads <matthewnads@gmail.com>
2021-01-17 06:13:22 -0500

    saved recipes

--- api/routes/pantry.js
+++ api/routes/pantry.js
@@ -144,4 +144,18 @@ router.get('/get_ingredients', function(req, res, next) {
     res.send(results.rows[0]);
   })
 });
+
+router.post('/save-recipe', function(req,res,next) {
+  var pool = new pg.Pool(config); 
+  var user_id = req.body.user_id; 
+  var recipe_id = req.body.recipe_id; 
+
+  pool.query('INSERT into saved_recipes (user_id, recipe_id) VALUES ($1,$2)',[user_id, recipe_id], (err, results)=>{
+    if (err) {
+      console.error('Error saving recipe: ', err);
+
+    }
+    res.send(results.rows[0]);
+  })
+});
 module.exports = router;
